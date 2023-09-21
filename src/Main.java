e252990fd39324cb4a50fda745b263b03564e238
c0mpl3x <marszemlya2312@gmail.com>
2023-09-21 13:42:41 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -116,12 +116,15 @@ public class Main {
         //Написати програму, яка читає з клавіатури два цілих числа
         //X та Y, після чого за допомогою тернарного оператора обраховує
         //вираз X^2-Y^2, якщо Y > 0 >= X, або X + Y в іншому випадку.
+
         Scanner scanner = new Scanner(System.in);
+        double res;
+        /*
         System.out.println("Введіть х: ");
         int x = scanner.nextInt();
         System.out.println("Введіть y: ");
         int y = scanner.nextInt();
-        double res;
+
         if (y > 0 && 0 >= x)
         {
             res = Math.pow(x, 2) - Math.pow(y, 2);
@@ -131,5 +134,31 @@ public class Main {
             res = x + y;
         }
         System.out.println("Результат: " + (int)res);
+        */
+
+        //Task 3
+        //Написати програму, яка читає з клавіатури рядок, що містить
+        //арифметичний вираз. Наприклад, "4 + 3", або "-2 -11". Програма
+        //повинна обрахувати значення виразу та перевірити, чи воно
+        //дорівнює нулю. Якщо дорівнює, програма повинна надрукувати
+        //"ТАК", або "НІ" у протилежному випадку.
+
+        String str = scanner.nextLine();
+        String[] tok = str.split(" ");
+        double a = Integer.parseInt(tok[0]);
+        double b = Integer.parseInt(tok[2]);
+        String  operand = tok[1];
+        System.out.println("0symbol: " + a + "\n1symbol: " + operand + "\n2symbol: " + b);
+        switch (operand)
+        {
+            case "+": a+=b; break;
+            case "-": a-=b; break;
+            case "*": a*=b; break;
+            case "/": if(b != 0) a/=b; else System.out.println("Помилка!!! На 0 не можна ділити!"); break;
+        }
+        //System.out.println("RES = " + a);
+        if (a == 0)
+            System.out.println("ТАК");
+        else System.out.println("НІ");
     }
 }
